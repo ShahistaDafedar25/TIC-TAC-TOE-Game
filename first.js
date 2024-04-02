ee863ecdb17e08020d787072e2fa861a1e839d68
ShahistaDafedar25 <dafedarshahista20@gmail.com>
2024-04-02 09:30:32 +0530

    Tic-Tac-Toe Game Project

---
+++ first.js
@@ -0,0 +1,91 @@
+// access the element of html by query selector
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true;//playerX,playerO
+
+//winning Pattern array
+const winPattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+
+];
+
+//reset btn function 
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+}
+
+// add O or X at each box and call checkwinner
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log("box was clicked");
+        if(turnO){
+            box.innerText="O";
+            box.setAttribute("style","color:blue");
+            turnO=false;
+        }
+        else{
+            box.innerText="X";
+            box.setAttribute("style","color:red");
+            turnO=true;
+        }
+        box.disabled =true;
+
+        checkWinnder();
+    })
+})
+
+
+//disable all the boxes
+const disableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+
+//enable all the boxes
+const enableBoxes = () =>{
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+};
+
+//show winner on browser
+const showWinner = (Winner)=>{
+    msg.innerText= `Congratulations, Winner is ${Winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+}
+
+//check Winner 
+const checkWinnder = () =>{
+    for(let pattern of winPattern){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val!="" && pos2Val!="" && pos3Val!=""){
+            if(pos1Val===pos2Val && pos2Val===pos3Val){
+                // console.log("Winner");
+                showWinner(pos1Val);
+            }
+        }
+    }
+};
+
+//event handler for reset and new game button
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
